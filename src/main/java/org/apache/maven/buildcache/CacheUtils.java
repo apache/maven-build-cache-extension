6343531331a15c4c0a5c00cde4db07fe4d95339a
Guillaume Nodet <gnodet@gmail.com>
2022-10-16 21:20:51 +0200

    Reformat

--- src/main/java/org/apache/maven/buildcache/CacheUtils.java
+++ src/main/java/org/apache/maven/buildcache/CacheUtils.java
@@ -212,8 +212,9 @@ public class CacheUtils
             while ( entry != null )
             {
                 Path file = out.resolve( entry.getName() );
-                if (!file.normalize().startsWith(out.normalize())) {
-                    throw new RuntimeException("Bad zip entry");
+                if ( !file.normalize().startsWith( out.normalize() ) )
+                {
+                    throw new RuntimeException( "Bad zip entry" );
                 }
                 if ( entry.isDirectory() )
                 {
