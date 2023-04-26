apache/maven-build-cache-extension

commit ac3e95ba3772142ad81205ab26ea4460c4348ba1

[MBUILDCACHE-57] Configure remote url/remote server id with command line property (#74)
* [MBUILDCACHE-57] Configure remote url/remote server id with command line property

---------

Signed-off-by: Olivier Lamy <olamy@apache.org>

src/main/java/org/apache/maven/buildcache/xml/CacheConfigImpl.java
@@ -83,6 +83,8 @@ public class CacheConfigImpl implements org.apache.maven.buildcache.xml.CacheCon
     public static final String CACHE_ENABLED_PROPERTY_NAME = "maven.build.cache.enabled";
     public static final String CACHE_LOCATION_PROPERTY_NAME = "maven.build.cache.location";
     public static final String REMOTE_ENABLED_PROPERTY_NAME = "maven.build.cache.remote.enabled";
+    public static final String REMOTE_URL_PROPERTY_NAME = "maven.build.cache.remote.url";
+    public static final String REMOTE_SERVER_ID_PROPERTY_NAME = "maven.build.cache.remote.server.id";
     public static final String SAVE_TO_REMOTE_PROPERTY_NAME = "maven.build.cache.remote.save.enabled";
     public static final String SAVE_NON_OVERRIDEABLE_NAME = "maven.build.cache.remote.save.final";
     public static final String FAIL_FAST_PROPERTY_NAME = "maven.build.cache.failFast";
@@ -504,13 +506,13 @@ public class CacheConfigImpl implements org.apache.maven.buildcache.xml.CacheCon
     @Override
     public String getId() {
         checkInitializedState();
-        return getRemote().getId();
+        return getProperty(REMOTE_SERVER_ID_PROPERTY_NAME, getRemote().getId());
     }
 
     @Override
     public String getUrl() {
         checkInitializedState();
-        return getRemote().getUrl();
+        return getProperty(REMOTE_URL_PROPERTY_NAME, getRemote().getUrl());
     }
 
     @Override

src/test/java/org/apache/maven/buildcache/its/RemoteCacheDavTest.java
@@ -62,6 +62,8 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import static org.apache.maven.buildcache.xml.CacheConfigImpl.REMOTE_SERVER_ID_PROPERTY_NAME;
+import static org.apache.maven.buildcache.xml.CacheConfigImpl.REMOTE_URL_PROPERTY_NAME;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -124,20 +126,19 @@ public class RemoteCacheDavTest {
     @AfterEach
     public void cleanup() throws Exception {
 
-        org.testcontainers.containers.Container.ExecResult result =
-                dav.execInContainer("ls", "-lrt", "/var/webdav/public/");
+        org.testcontainers.containers.Container.ExecResult result = dav.execInContainer("ls", "-lrt", "/var/webdav");
 
         LOGGER.info("before clean in container result: {}", result);
 
-        result = dav.execInContainer("rm", "-rf", "/var/webdav/public");
+        result = dav.execInContainer("rm", "-rf", "/var/webdav");
 
         LOGGER.info("clean in container result: {}", result);
 
-        result = dav.execInContainer("ls", "-lrt", "/var/webdav/");
+        result = dav.execInContainer("ls", "-lrt", "/var/webdav");
 
         LOGGER.info("after clean in container result: {}", result);
 
-        cleanDirs(localCache, remoteCache.resolve("mbce"));
+        cleanDirs(localCache);
 
         dav.close();
     }
@@ -178,7 +179,7 @@ public class RemoteCacheDavTest {
         assertTrue(hasBuildInfoXml(localCache), () -> error(localCache, "local", true));
         assertFalse(hasBuildInfoXml(remoteCache), () -> error(remoteCache, "remote", false));
 
-        cleanDirs(localCache, remoteCache.resolve("mbce"));
+        cleanDirs(localCache);
 
         verifier.getCliOptions().clear();
         verifier.addCliOption("--settings=" + settings);
@@ -200,6 +201,40 @@ public class RemoteCacheDavTest {
         verifier.addCliOption("-D" + WAGON_TRANSPORT_PRIORITY + "=" + ("wagon".equals(transport) ? "10" : "0"));
         verifier.addCliOption("-D" + MAVEN_BUILD_CACHE_REMOTE_SAVE_ENABLED + "=false");
         verifier.setLogFileName("../log-3.txt");
+        verifier.executeGoals(Arrays.asList("clean", "install"));
+        verifier.verifyErrorFreeLog();
+
+        assertTrue(hasBuildInfoXml(localCache), () -> error(localCache, "local", true));
+        assertTrue(hasBuildInfoXml(remoteCache), () -> error(remoteCache, "remote", true));
+
+        // replace url and server id with a bad one to be sure cli property is used
+        substitute(
+                basedir.resolve(".mvn/maven-build-cache-config.xml"),
+                "url",
+                "http://foo.com",
+                "id",
+                "foo",
+                "location",
+                localCache.toString());
+
+        cleanDirs(localCache);
+        try {
+            // depending on uid used for execution but can be different from the one using docker and so different file
+            // permissions..
+            dav.execInContainer("rm", "-rf", "/var/webdav/public/*");
+        } catch (InterruptedException e) {
+            throw new IOException("cannot delete remote cache");
+        }
+
+        verifier.getCliOptions().clear();
+        verifier.addCliOption("--settings=" + settings);
+        verifier.addCliOption("-X");
+        verifier.addCliOption("-D" + HTTP_TRANSPORT_PRIORITY + "=" + ("wagon".equals(transport) ? "0" : "10"));
+        verifier.addCliOption("-D" + WAGON_TRANSPORT_PRIORITY + "=" + ("wagon".equals(transport) ? "10" : "0"));
+        verifier.addCliOption("-D" + MAVEN_BUILD_CACHE_REMOTE_SAVE_ENABLED + "=true");
+        verifier.setSystemProperty(REMOTE_URL_PROPERTY_NAME, url);
+        verifier.setSystemProperty(REMOTE_SERVER_ID_PROPERTY_NAME, REPO_ID);
+        verifier.setLogFileName("../log-4.txt");
         verifier.executeGoals(Arrays.asList("clean", "install"));
         verifier.verifyErrorFreeLog();
 
@@ -229,6 +264,7 @@ public class RemoteCacheDavTest {
         for (int i = 0; i < strings.length / 2; i++) {
             str = str.replaceAll(Pattern.quote("${" + strings[i * 2] + "}"), strings[i * 2 + 1]);
         }
+        Files.deleteIfExists(path);
         Files.write(path, str.getBytes(StandardCharsets.UTF_8));
     }
 

commit 0410dc42c8fc3b7d06f982e2e46db41558e72145

fix issue introduced by https://github.com/apache/maven-build-cache-extension/commit/ac3e95ba3772142ad81205ab26ea4460c4348ba1 remote enabled does not check property
Signed-off-by: Olivier Lamy <olamy@apache.org>

src/main/java/org/apache/maven/buildcache/xml/CacheConfigImpl.java
@@ -453,7 +453,7 @@ public class CacheConfigImpl implements org.apache.maven.buildcache.xml.CacheCon
     @Override
     public boolean isRemoteCacheEnabled() {
         checkInitializedState();
-        return getRemote().getUrl() != null
+        return getUrl() != null
                 && getProperty(REMOTE_ENABLED_PROPERTY_NAME, getRemote().isEnabled());
     }
 