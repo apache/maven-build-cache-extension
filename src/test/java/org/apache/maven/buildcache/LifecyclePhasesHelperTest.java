25d6c819101a8a21f274d14b848822105d10bd15
Elliotte Rusty Harold <elharo@ibiblio.org>
2026-02-13 07:32:22 -0500

    Lists

--- src/test/java/org/apache/maven/buildcache/LifecyclePhasesHelperTest.java
+++ src/test/java/org/apache/maven/buildcache/LifecyclePhasesHelperTest.java
@@ -231,7 +231,7 @@ class LifecyclePhasesHelperTest {
 
         List<MojoExecution> cachedSegment = lifecyclePhasesHelper.getCachedSegment(projectMock, mojoExecutions, build);
 
-        assertIterableEquals(Lists.newArrayList(compile, test), cachedSegment);
+        assertIterableEquals(Arrays.asList(compile, test), cachedSegment);
     }
 
     @Test
